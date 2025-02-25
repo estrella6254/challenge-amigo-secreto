9378ec5a9c34dd745790d92c88b8bd1e9970648d
estrella6254 <veeduriapopayan@gmail.com>
2025-02-24 21:16:58 -0500

    llamado a la funcion actualizar lista dentro de la funcion agregar amigo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+    // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 //crear la lista para amigos
 let amigos = [];
@@ -11,18 +11,19 @@ function agregarAmigo() {
         return;
     }
     amigos.push(nombreInput);
+    actualizarLista(); // declarar esta funcion aqui hace que la lista se actualice y se refleje en la pantalla
     document.getElementById("amigo").value = "";
 }
 
 //funcion actualizar y mostrar amigos en pantalla 
-function actualizarLista() {   
+function actualizarLista() { 
     const listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = "";
     amigos.forEach((amigo) => {
         const li = document.createElement("li");
         li.textContent = amigo;
         listaAmigos.appendChild(li);
-    })
+    });
 }
 
 //funcion para el sorteo de amigos
